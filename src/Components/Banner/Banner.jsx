753821246861e80c3ace0c4bf230f6a0c978a26d
ArafatSani10 <mdarafatrahmansani17@gmail.com>
2025-03-02 21:40:05 +0600

    Milolyn card create done.

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='w-full max-sm:h-[auto] mx-auto bg-gradient-to-r from-blue-800 via-blue-400 to-blue-800 py-20 overflow-hidden'>
-            <div className='flex flex-col items-center justify-center md:flex-row'>
+        <div className='w-full max-sm:h-[550px] mx-auto bg-gradient-to-r from-blue-800 via-blue-400 to-blue-900 py-20 opacity-85 overflow-hidden'>
+            <div className='flex flex-col  items-center justify-center md:flex-row'>
                 {/* Banner Image */}
                 <img
                     src="https://i.ibb.co/jvW8z8wr/download-1.png"
                     alt="Metabolic Power"
-                    className='opacity-90 mb-10 md:mb-0 max-sm:w-[150px] mx-auto md:mx-0'
+                    className='opacity-90 mb-10 md:mb-0 max-sm:w-[130px] mx-auto md:mx-0'
                 />
 
                 {/* Banner Text */}
-                <div className='text-center md:text-left opacity-95 md:mt-0 md:ml-28'>
-                    <h1 className='text-4xl sm:text-5xl md:text-[60px] lg:text-[75px] font-bold text-white font-oswald'>
+                <div className='text-center md:text-left -mb-40  opacity-95 md:mt-0 md:ml-28'>
+                    <h1 className='text-5xl sm:text-5xl md:text-[60px] lg:text-[75px] font-bold text-white font-oswald'>
                         Unlock Your
                     </h1>
-                    <span className='text-4xl sm:text-5xl md:text-[60px] lg:text-[75px] font-bold text-white font-oswald'>
+                    <span className='text-5xl sm:text-5xl md:text-[60px] lg:text-[75px] font-bold text-white font-oswald'>
                         Metabolic Power
                     </span>
 
@@ -27,7 +27,7 @@ const Banner = () => {
             </div>
 
             {/* People and Star Section - Aligned to the left */}
-            <div className='w-full max-sm:hidden mx-auto mt-12 flex mx-52'>
+            <div className='w-full max-sm:hidden   mt-12  flex lg:mx-72'>
                 <img className='w-[400px]' src="https://i.ibb.co/PsCVN3Z0/download-4.png" alt="People and Star" />
             </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
+import MitolynCard from '../../Components/MitolynCard/MitolynCard';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <MitolynCard></MitolynCard>
         </div>
     );
 };
